b4c5515366e7bd5dc1e8b5356cf6b73454c8a911
Kofler Daniel <Daniel.Kofler2@edu.fh-kaernten.ac.at>
2021-12-28 11:17:58 +0100

    fixed Bugs, further optimized plot3d function

--- functions/function_clusterPlot3d.py
+++ functions/function_clusterPlot3d.py
@@ -1,69 +1,69 @@
-def plot(assignmentDict, title):
+def plot(assignmentDict, title, xLabel, yLabel, zLabel):
     
     from matplotlib import pyplot as plt
     import numpy as np, random
 
     fig = plt.figure(figsize=(10,10))
     ax = fig.add_subplot(projection='3d')
-    ax.set_xlabel('x')
-    ax.set_xlabel('y')
-    ax.set_xlabel('z')
+    ax.set_xlabel(xLabel)
+    ax.set_ylabel(yLabel)
+    ax.set_zlabel(zLabel)
 
+    dataPoints = []
     clusterCenters = []
+    
+    for a in assignmentDict: 
+        if (a in dataPoints): #get data points from dict
+            dataPoints.append(a)
+
+        if (assignmentDict[a] not in clusterCenters): #get cluster centers from dict
+            clusterCenters.append(assignmentDict[a])     
+
     clusterColors = {}
 
-    for a in assignmentDict: #get cluster centers from dict
-        if (assignmentDict[a] not in clusterCenters):
-            clusterCenters.append(assignmentDict[a])
-
-    for c in clusterCenters: #assign random colors to clusters
+    for c in clusterCenters: #assign random colors to each cluster
         numbers = [random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)]
         clusterColors[c] = '#' + ''.join('{:02X}'.format(n) for n in numbers)
 
-    X_C = []
-    Y_C = []
-    Z_C = []
-
-    for x,y,z in clusterCenters:
-        X_C.append(x)
-        Y_C.append(y)
-        Z_C.append(z)
-
-    for i,c in enumerate(clusterCenters):
-        ax.scatter(X_C[i], Y_C[i], Z_C[i], s = 200, c = clusterColors[c], marker = "x")
-
-    dataPoints = []
     X_D = []
     Y_D = []
     Z_D = []
-
-    for a in assignmentDict: #get data points from dict
-        if (a in dataPoints):
-            dataPoints.append(a)
+    X_C = []
+    Y_C = []
+    Z_C = []
 
     for x,y,z in dataPoints:
         X_D.append(x)
         Y_D.append(y)
         Z_D.append(z)
 
+    for x,y,z in clusterCenters:
+        X_C.append(x)
+        Y_C.append(y)
+        Z_C.append(z)
+
     pts = []
 
-    for c in clusterCenters:
+    for i,c in enumerate(clusterCenters):
+        #plot clusters
+        ax.scatter(X_C[i], Y_C[i], Z_C[i], s = 200, c = clusterColors[c], marker = "x", alpha = 1, depthshade = False)
+
+        # plot data points
         pts.clear()
         X_D.clear()
         Y_D.clear()
         Z_D.clear()
-
-        for a in assignmentDict:
+    
+        for a in assignmentDict: # get datapoints assigned to current cluster
             if assignmentDict[a] == c:
                 pts.append(a)
-
+        
         for x,y,z in pts:
             X_D.append(x)
             Y_D.append(y)
             Z_D.append(z)
 
-        ax.scatter(X_D, Y_D, Z_D, s = 30, c = clusterColors[c], marker = "o")
+        ax.scatter(X_D, Y_D, Z_D, s = 30, c = clusterColors[c], marker = "o", alpha = 1, depthshade = False)
 
     plt.title(title)
     plt.show()

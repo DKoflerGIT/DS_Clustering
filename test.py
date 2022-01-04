0812004968b90c5d05ff45c59290c0cc9ede6bab
DKoflerGIT <74835806+DKoflerGIT@users.noreply.github.com>
2022-01-04 12:01:56 +0100

    file cleanup

--- test.py
+++
@@ -1,7 +0,0 @@
-from functions import kMeans as km
-
-dpts = []
-dpts = km.createRandomDatapoints(500, 2) #kMeans works for 2+ dimensions, for 1 dimension not always. plot works for 2 or 3 dimensions
-#clusterAmount = km.findOptimalClusterAmount(dpts)
-kmeans = km.AssignClusters(dpts, 5, False)
-km.plotClusters(kmeans)
